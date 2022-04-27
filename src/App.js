72eccf167f9dd5b6f0c1dc0f6d7b878144f9e127
Ugochukwu Nwadike <nwadikeugochukwu93@gmail.com>
2022-04-27 14:06:42 +0100

    changes on App.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 
 import './App.css';
-// import ContactPage from './pages/ContactPage';
 import Products from './pages/Products';
 
 function App() {
   return (
     <div className="App">
-      <Products/>
-      {/* <ContactPage/> */}
     </div>
   );
 }
